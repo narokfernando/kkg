eb0f5c4820b07f8219534b4272da3bd4a6466ebc
Narok <narokfernando@gmail.com>
2018-11-17 17:00:50 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,1 @@
-hello kkg world
-
-<?php
-
-// Muestra toda la información, por defecto INFO_ALL
-phpinfo();
-
-// Muestra solamente la información de los módulos.
-// phpinfo(8) hace exactamente lo mismo.
-phpinfo(INFO_MODULES);
-
-?>
+kkg
